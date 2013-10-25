9bde16436bec1cfcf6fca7ea90fc742eaaba524c
golddiamonds <rhall@berkeley.edu>
2013-10-25 11:10:19 -0700

    Added a scanner for player input. Also created a loop that the player cannot yet get out of :)

--- src/theotherfoot/Main.java
+++ src/theotherfoot/Main.java
@@ -1,12 +1,21 @@
 package theotherfoot;
 
+import java.util.Scanner;
+
 public class Main {
 
 	
 	public static void main(String[] args) {
 	
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to 'The Other Foot'");
-
+		
+		boolean run = true;
+		while (run){
+			System.out.println("What next?");
+			scanner.next();
+		}
+		
 	}
 
 }
